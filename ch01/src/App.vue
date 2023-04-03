0928978621b2e83c7c7a61bb42f6a44556eee4ee
sorkcoavus <sorkcoavus@gmail.com>
2023-04-03 12:46:12 +0900

    2023/04/03

---
+++ ch01/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <h3>Ch01.Hello Vue</h3>
+  <p>
+     Hello World<br>
+     Hello Vue
+     </p>
+</template>
+
+<script>
+/**
+ * 날짜 : 2023/04/03
+ * 이름 : 김경준
+ * 내용 : Ch01. HelloVue 실습
+ * 
+ * 환경설정
+ * 1) node.js 설치
+ * 2) node.js 환경변수 등록
+ * 3) VSCode 확장팩 설치
+ *    - volar, vue3 snippets, prettier, ESLint
+ * 
+ * 프로젝트 생성/실행
+ * - npm create ch01
+ * - cd ch01
+ * - npm run serve
+ * 
+ */
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+
+</style>
