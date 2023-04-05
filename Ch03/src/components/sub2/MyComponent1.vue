aa9925910aa836612f520b1f5032a6cedc71ad14
sorkcoavus <sorkcoavus@gmail.com>
2023-04-05 11:48:38 +0900

    4/5

---
+++ Ch03/src/components/sub2/MyComponent1.vue
@@ -0,0 +1,22 @@
+<template>
+  <h4>MyComponent1 - data Options API</h4>
+  <p>
+    이름 : {{ name }}<br />
+    나이 : {{ age }}<br />
+    주소 : {{ addr }}
+  </p>
+</template>
+<script>
+export default {
+  name: "Mycomponent1",
+  // 컴포넌트에서 사용할 data(상태값) 선언하는 API
+  data() {
+    return {
+      name: "홍길동",
+      age: 21,
+      addr: "부산",
+    };
+  },
+};
+</script>
+<style scoped></style>
